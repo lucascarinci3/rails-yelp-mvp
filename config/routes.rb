b85948de6e20cda9c68a1befa9cc758ae658d230
Luca Scarinci <luca.scarinci3@gmail.com>
2017-11-08 16:28:15 +0100

    Last step

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,31 @@
 Rails.application.routes.draw do
-  root to: "controller#action"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+
+  # View
+  # get 'restaurants', to: 'restaurants#index'
+
+  # Create
+  # get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # Show
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # Create
+  # get 'reviews/new', to: 'reviews#new', as: 'new_review'
+  # post 'reviews', to: 'reviews#create'
+
 end
+
+  # Admin
+
+  # Edit
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # Delete
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+20.times do
+  Restaurant.create( name: Faker::Simpsons.character,
+                     address: Faker::Simpsons.location,
+                     category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
